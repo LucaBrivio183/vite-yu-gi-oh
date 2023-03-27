c5c64ed5f9a9424edea1684138d945cdab93141c
LucaBrivio183 <l.brivio4@gmail.com>
2023-03-27 17:22:13 +0200

    feat: first scaffolding

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,16 @@ import axios from 'axios';
 //function
 import { store } from './store';
 //components
+import AppHeader from './assets/components/header/AppHeader.vue';
+import AppMain from './assets/components/main/AppMain.vue';
+import AppFooter from './assets/components/footer/AppFooter.vue';
+
 export default {
   name: 'App',
   components: {
+    AppHeader,
+    AppMain,
+    AppFooter
   },
   data() {
     return {
---
+++ src/assets/components/footer/AppFooter.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+    name: 'Footer'
+}
+</script>
+
+<template>
+    <Footer>
+
+    </Footer>
+</template>
+
+
+
+<style lang="scss" scoped></style>
